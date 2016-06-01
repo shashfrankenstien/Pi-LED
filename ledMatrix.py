c6ed9fe876f702641b03ea3efe53beb929fa0d34
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2016-06-01 07:19:08 -0400

    some changes

--- ledMatrix.py
+++ ledMatrix.py
@@ -7,6 +7,23 @@ def blink(i):
         GPIO.output(i, GPIO.LOW)
         return 0
 
+def rain(gnds, pins):
+        while True:
+                for gnd in gnds:
+                        GPIO.setmode(GPIO.BOARD)
+                        GPIO.setup(gnd, GPIO.OUT)
+                        GPIO.output(gnd, GPIO.LOW)
+                        for pin in pins:
+                                GPIO.setup(pin, GPIO.OUT)
+                                GPIO.output(pin, GPIO.HIGH)
+                                time.sleep(0.01)
+                                GPIO.output(pin, GPIO.LOW)
+                                blink(pin)
+                        GPIO.cleanup()
+                        time.sleep(0.5)
+                #time.sleep(1)
+
+
 GPIO.setmode(GPIO.BOARD)
 pins = [29, 31, 33, 35, 37, 36, 38, 40]
 gnds = [7, 13, 11]
@@ -14,17 +31,7 @@ pinsRev = pins[::-1]
 
 # GPIO.setup(7, GPIO.OUT)
 # GPIO.output(7, GPIO.LOW)
-while True:
-        for gnd in gnds:
-                GPIO.setmode(GPIO.BOARD)
-                GPIO.setup(gnd, GPIO.OUT)
-                GPIO.output(gnd, GPIO.LOW)
-                for pin in pins:
-                        GPIO.setup(pin, GPIO.OUT)
-                        GPIO.output(pin, GPIO.HIGH)
-                        time.sleep(0.01)
-                        GPIO.output(pin, GPIO.LOW)
-                GPIO.cleanup()
-		time.sleep(0.5)
-        #time.sleep(1)
+
+rain(gnds, pins)
+
 GPIO.cleanup()
