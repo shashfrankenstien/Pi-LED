cf8ebba9f763387529099e7a0ff9a3ba6041d30a
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2016-06-01 09:08:08 -0400

    Hooo

--- ledMatrix.py
+++ ledMatrix.py
@@ -50,16 +50,14 @@ pin_config = {0:(29, 7), 1:(29, 11), 2:(29, 13),
                 21:(40, 7), 22:(40, 11), 23:(40, 13)}
 # rain(gnds, pins)
 x = 0
-while x < 24:
+while x < 23:
         each_high(pin_config[x])
         each_high(pin_config[x+1])
-        each_high(pin_config[x+2])
-        each_high(pin_config[x+3])
+
         time.sleep(0.5)
         each_low(pin_config[x])
         each_low(pin_config[x+1])
-        each_low(pin_config[x+2])
-        each_low(pin_config[x+3])
+
 # GPIO.cleanup()
 
 
